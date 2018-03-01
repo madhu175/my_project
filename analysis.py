1bb6a6be8e8b50a877d5a96fbd95df71c8ef0e3c
Madhu <madhu.kappagantu@wsu.edu>
2018-03-01 11:26:19 -0800

    creat .py script

--- analysis.py
+++ analysis.py
@@ -4,6 +4,9 @@ Spyder Editor
 
 This is a temporary script file.
 """
+#working on oceania data
 
 import pandas as pd
-dat = pd.read_csv('python-novice-gapminder-data/gapminder_gdp_oceania.csv')
+
+oceania = pd.read_csv('python-novice-gapminder-data/gapminder_gdp_oceania.csv')
+
