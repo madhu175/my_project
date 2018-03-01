44f91a3329440f317b1c4fd2a9cdbc01da955f67
Madhu <madhu.kappagantu@wsu.edu>
2018-03-01 10:29:18 -0800

    creat .py script

--- analysis.py
+++ analysis.py
@@ -6,4 +6,4 @@ This is a temporary script file.
 """
 
 import pandas as pd
-dat = pd.read_csv('python-novice-gapminder-data/gapminder_gdp_europe.csv')
+dat = pd.read_csv('python-novice-gapminder-data/gapminder_gdp_oceania.csv')
