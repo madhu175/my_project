9a489fcd5300f54f31f3c7fb69a210e1813df9bd
Madhu <madhu.kappagantu@wsu.edu>
2018-03-01 10:12:27 -0800

    creat .py script

---
+++ analysis.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import pandas as pd
+dat = pd.read_csv('python-novice-gapminder-data/gapminder_gdp_europe.csv')
